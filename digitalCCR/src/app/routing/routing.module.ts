c802ee3db4609b9a5aca71a98695a0caeed2d80c
tartarus762 <brett.jeffreson@gmail.com>
2019-07-20 15:08:24 +1000

    Forgot to save

--- digitalCCR/src/app/routing/routing.module.ts
+++ digitalCCR/src/app/routing/routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router'
+import {LoginComponent} from '../login/login.component'
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent}
+  // {path: 'members', component: EditMemberComponent, outlet: "dashboard"}
+]
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports :[
+    RouterModule.forRoot(routes, {useHash: true})
   ],
-  declarations: []
+  exports: [
+    RouterModule
+  ]
 })
 export class RoutingModule { }
