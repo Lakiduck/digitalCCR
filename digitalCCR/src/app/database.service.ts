419bc5a4c3ba518b5872c18fc0742a368f697c65
tartarus762 <brett.jeffreson@gmail.com>
2019-07-20 16:28:36 +1000

    Added basic database functionality

--- digitalCCR/src/app/database.service.ts
+++ digitalCCR/src/app/database.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -16,7 +16,27 @@ export class DatabaseService {
   }
 
   getPatientById(id: string){
-    return this.firestore.collection('patients', ref => ref.where('id', '==', id))
+    return this.firestore.collection('patients').doc(id).get().pipe(map(a => ({data: a.data()})));
+  }
+
+  getPatient(patient: DocumentReference){
+    return this.firestore.collection("medication").doc(patient.id.trim()).get().pipe(map(a => ({data: a.data()})));
+  }
+
+  getMedication(medication: DocumentReference){      
+      return this.firestore.collection("medication").doc(medication.id.trim()).get().pipe(map(a => ({data: a.data()})));  
+  }
+
+  getMedicationByName(name: string){
+    return this.firestore.collection('patients', ref => ref.where('name', '==', name))
     .snapshotChanges().pipe(map(actions => actions.map(a => ({ data: a.payload.doc.data() }))));
+  }
+
+  getMedicationById(id: string){
+    return this.firestore.collection('medication').doc(id.trim()).get().pipe(map(a => ({data: a.data()})));
+  }
+
+  getAppointment(appointment: DocumentReference){
+    return this.firestore.collection("medication").doc(appointment.id.trim()).get().pipe(map(a => ({data: a.data()})));
   }
 }
