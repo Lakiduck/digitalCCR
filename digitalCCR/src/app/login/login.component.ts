51b2b88809b438a1a92f98419f002ef03e90c499
tartarus762 <brett.jeffreson@gmail.com>
2019-07-21 15:46:09 +1000

    Added new login screen

--- digitalCCR/src/app/login/login.component.ts
+++ digitalCCR/src/app/login/login.component.ts
@@ -1,50 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthStateService } from '../auth-state.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
     private hide: Boolean = true;
 
-  loginForm = new FormGroup({
-      email: new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl('',[Validators.required])
-  });
+    loading = false;
 
-  private errMsg: string;
+    loginForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required])
+    });
 
-  constructor(private AuthState: AuthStateService, private router: Router, private snackbar: MatSnackBar) {
+    private errMsg: string;
 
-   }
+    constructor(private AuthState: AuthStateService, private router: Router, private snackbar: MatSnackBar) {
 
-   loginSubmit(){
-     this.AuthState.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password).subscribe(
-        (data) => {
-            this.router.navigate(['/dashboard']);
-          },
-          (error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            if (errorCode === 'auth/wrong-password') {
-              this.errMsg = 'Wrong email/password.';
-            } else {
-              this.errMsg = 'Wrong email/password.';
-              //error.message;
+    }
+
+    loginSubmit() {
+        this.loading = true;
+        this.AuthState.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password).subscribe(
+            (data) => {
+                this.loading = false;
+                this.router.navigate(['/dashboard']);
+            },
+            (error) => {
+                this.loading = false;
+                // Handle Errors here.
+                var errorCode = error.code;
+                if (errorCode === 'auth/wrong-password') {
+                    this.errMsg = 'Wrong email/password.';
+                } else {
+                    this.errMsg = 'Wrong email/password.';
+                    //error.message;
+                }
+                this.snackbar.open(this.errMsg, "dismiss", { duration: 3000 });
             }
-            this.snackbar.open(this.errMsg, "dismiss",{duration: 3000});
-          }
-     );
-   }
+        );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
